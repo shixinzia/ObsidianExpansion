85bf07abdd6a1d954e3200319c8f47e5c226a735
haiman <83174104+haiman233@users.noreply.github.com>
2022-05-15 15:33:04 +0800

    汉化

--- src/main/java/me/lucasgithuber/obsidianexpansion/utils/Categories.java
+++ src/main/java/me/lucasgithuber/obsidianexpansion/utils/Categories.java
@@ -17,40 +17,40 @@ public class Categories {
     public static final ItemGroup OMC_RESOURCES = new SubGroup(
             "omc_resources",
             new CustomItemStack(new ItemStack(Material.OBSIDIAN), BukkitComponentSerializer.legacy().serialize
-                    (ObsidianExpansion.MM.deserialize("<gradient:#8100D1:#D320FC>Recursos</gradient>")))
+                    (ObsidianExpansion.MM.deserialize("<gradient:#8100D1:#D320FC>资源</gradient>")))
     );
     public static final ItemGroup OMC_ITEMS = new SubGroup(
             "omc_items",
             new CustomItemStack(new ItemStack(Material.NETHERITE_INGOT), BukkitComponentSerializer.legacy().serialize
-                    (ObsidianExpansion.MM.deserialize("<gradient:#8100D1:#D320FC>Items</gradient>")))
+                    (ObsidianExpansion.MM.deserialize("<gradient:#8100D1:#D320FC>物品</gradient>")))
     );
     public static final ItemGroup OMC_MACHINES = new SubGroup(
             "omc_machines",
             new CustomItemStack(new ItemStack(Material.CHISELED_POLISHED_BLACKSTONE), BukkitComponentSerializer.legacy().serialize
-                    (ObsidianExpansion.MM.deserialize("<gradient:#8100D1:#D320FC>Maquinas</gradient>")))
+                    (ObsidianExpansion.MM.deserialize("<gradient:#8100D1:#D320FC>机器</gradient>")))
     );
     public static final ItemGroup OMC_GENERATORS = new SubGroup(
             "omc_generators",
             new CustomItemStack(new ItemStack(Material.BLACKSTONE_WALL), BukkitComponentSerializer.legacy().serialize
-                    (ObsidianExpansion.MM.deserialize("<gradient:#8100D1:#D320FC>Geradores</gradient>")))
+                    (ObsidianExpansion.MM.deserialize("<gradient:#8100D1:#D320FC>材料发生器</gradient>")))
     );
     public static final ItemGroup OMC_DROPS = new SubGroup(
             "omc_drops",
             new CustomItemStack(new ItemStack(Material.NETHERITE_SWORD), BukkitComponentSerializer.legacy().serialize
-                    (ObsidianExpansion.MM.deserialize("<gradient:#8100D1:#D320FC>Drops</gradient>")))
+                    (ObsidianExpansion.MM.deserialize("<gradient:#8100D1:#D320FC>生物掉落物</gradient>")))
     );
     public static final ItemGroup OMC_FORGE_GROUP = new ObsidianForgeGroup(
             ObsidianExpansion.createKey("junction_category"),
             new CustomItemStack(Material.SMITHING_TABLE, BukkitComponentSerializer.legacy().serialize
-                    (ObsidianExpansion.MM.deserialize("<gradient:#8100D1:#D320FC>Forjas</gradient>"))),3);
+                    (ObsidianExpansion.MM.deserialize("<gradient:#8100D1:#D320FC>锻造</gradient>"))),3);
     public static final ItemGroup OMC_MAIN = new MultiGroup(
             "omc_expansion",
             new CustomItemStack(Material.CRYING_OBSIDIAN, BukkitComponentSerializer.legacy().serialize
-                    (ObsidianExpansion.MM.deserialize("<gradient:#8100D1:#D320FC>Obsidian Expansion</gradient>"))),
+                    (ObsidianExpansion.MM.deserialize("<gradient:#8100D1:#D320FC>黑曜石科技</gradient>"))),
             OMC_RESOURCES, OMC_ITEMS, OMC_MACHINES, OMC_GENERATORS, OMC_DROPS, OMC_FORGE_GROUP
     );
     public static final ItemGroup OMC_FORGE_CHEAT = new SubGroup("omc_forge_cheat",
-            new CustomItemStack(Material.SMITHING_TABLE, "&bForjas &c- RECEITAS INCORRETAS"));
+            new CustomItemStack(Material.SMITHING_TABLE, "&b锻造 &7- &c作弊模式"));
     public static void setup(ObsidianExpansion omc){
         OMC_FORGE_GROUP.register(omc);
         OMC_MAIN.register(omc);
